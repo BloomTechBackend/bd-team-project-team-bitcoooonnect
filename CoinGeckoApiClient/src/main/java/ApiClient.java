602123ca9edd8c077354c12a07799b3d6e077549
Ron Walker <ron22@hey.com>
2022-02-17 12:07:28 -0800

    add coin.json

--- CoinGeckoApiClient/src/main/java/ApiClient.java
+++ CoinGeckoApiClient/src/main/java/ApiClient.java
@@ -1,14 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
-import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +27,6 @@ public class ApiClient {
     public static void main(String[] args) throws Exception {
         run();
     }
-
     public static void run() throws Exception {
         final OkHttpClient client = new OkHttpClient();
         final Moshi moshi = new Moshi.Builder().build();
@@ -43,4 +52,39 @@ public class ApiClient {
         }
     }
 
+    public static void initCoinList() throws Exception {
+        final OkHttpClient client = new OkHttpClient();
+        final Moshi moshi = new Moshi.Builder().build();
+        Type type = Types.newParameterizedType(List.class, Map.class);
+        final JsonAdapter<List<Map<String, Object>>> jsonAdapter =
+                moshi.adapter(type);
+        Request request = new Request.Builder()
+                .url("https://api.coingecko" +
+                        ".com/api/v3/coins/markets?vs_currency=usd&per_page" +
+                        "=250")
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            List<Map<String, Object>> gist =
+                    jsonAdapter.fromJson(response.body().source());
+
+            String filename = "src/main/cloudformation/coin.json";
+            JSONObject coinsObject = new JSONObject();
+
+            JSONArray coinsList = new JSONArray();
+
+            coinsObject.put("coins", coinsList);
+
+            for (Map<String, Object> coin : gist) {
+                JSONObject sampleObject3 = new JSONObject();
+                sampleObject3.put("id", coin.get("symbol"));
+                sampleObject3.put("name", coin.get("id"));
+                sampleObject3.put("price", coin.get("current_price"));
+
+                coinsList.add(sampleObject3);
+            }
+            Files.write(Paths.get(filename), coinsObject.toJSONString().getBytes());
+        }
+    }
 }
---
+++ CoinGeckoApiClient/src/main/java/Coins.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Coins {
+    private List<Coin> coins;
+
+    public Coins(List<Coin> coins) {
+        this.coins = coins;
+    }
+
+    public Coins() {}
+}
