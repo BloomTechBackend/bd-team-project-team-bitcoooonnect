390c7ff7b92c4ef40407747414055ac43e95eee3
Ron Walker <ron22@hey.com>
2022-02-18 14:05:35 -0800

    delete stack before adding new stack

--- Bitcoooonnect/configurations/cloudformation/createProjectMacOnly.bat
+++ Bitcoooonnect/configurations/cloudformation/createProjectMacOnly.bat
@@ -1,3 +1,7 @@
+aws cloudformation delete-stack --region us-east-2 --stack-name cryptoservice-createtables
+
+sleep 60
+
 aws cloudformation create-stack --region us-east-2 --stack-name cryptoservice-createtables --template-body file://tables.template.yml --capabilities CAPABILITY_IAM
 
 sleep 10
--- Bitcoooonnect/configurations/cloudformation/createProjectPC.bat
+++ Bitcoooonnect/configurations/cloudformation/createProjectPC.bat
@@ -1,3 +1,7 @@
+aws cloudformation delete-stack --region us-east-2 --stack-name cryptoservice-createtables
+
+timeout /t 60
+
 aws cloudformation create-stack --region us-east-2 --stack-name cryptoservice-createtables --template-body file://tables.template.yml --capabilities CAPABILITY_IAM
 
 timeout /t 10
