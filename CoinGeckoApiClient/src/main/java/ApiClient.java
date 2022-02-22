4df1d3e47254b212a2ed3bbe20b87b943606af58
Ron Walker <ron22@hey.com>
2022-02-22 10:13:50 -0800

    update coin prices

--- CoinGeckoApiClient/src/main/java/ApiClient.java
+++ CoinGeckoApiClient/src/main/java/ApiClient.java
@@ -1,106 +1,72 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
-
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.simple.JSONArray;
+import okhttp3.*;
 import org.json.simple.JSONObject;
-
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
 
 public class ApiClient {
-    public static void main(String[] args) throws Exception {
-//        runAllInitCoinList();
-    }
-    public static void run() throws Exception {
-        final OkHttpClient client = new OkHttpClient();
-        final Moshi moshi = new Moshi.Builder().build();
-        Type type = Types.newParameterizedType(List.class, Map.class);
-        final JsonAdapter<List<Map<String, Object>>> jsonAdapter =
-                moshi.adapter(type);
-        Request request = new Request.Builder()
-                .url("https://api.coingecko" +
-                        ".com/api/v3/coins/markets?vs_currency=usd&per_page" +
-                        "=250")
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    public static final OkHttpClient client = new OkHttpClient();
+
+    public static void updateCoinPrice() throws Exception {
+
+        JSONObject coinsObject = new JSONObject();
+        JSONObject coinsListObject = new JSONObject();
+
+        Request request1 = new Request.Builder()
+                .url("https://pro-api.coinmarketcap.com/v1/cryptocurrency/" +
+                        "listings/latest?start=1&limit=5000&convert=USD")
+                .header("X-CMC_PRO_API_KEY", "f8137c54-5648-4e3e-92eb-9e64a4e36b4e")
                 .build();
+
+        final Moshi moshi = new Moshi.Builder().build();
+        Type type = Types.newParameterizedType(Map.class, String.class,
+                Object.class);
+
+        final JsonAdapter<Map<String, Object>> jsonAdapter =
+                moshi.adapter(type);
+
+        Map<String, Object> gist1;
+        List<Map<String, Object>> gist2;
+        try (Response response = client.newCall(request1).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            gist1 = jsonAdapter.fromJson(response.body().source());
+            gist2 = (List) gist1.get("data");
+
+            System.out.println("size :" + gist2.size());
+
+
+            for (Map<String, Object> coin : gist2) {
+
+                Map<String, Map<String, Object>> quote = (Map)coin.get("quote");
+
+                coinsListObject.put(coin.get("name"), quote.get("USD").get(
+                        "price"));
+            }
+        }
+        coinsObject.put("coins", coinsListObject);
+
+        RequestBody body = RequestBody.create(coinsObject.toJSONString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://t3d210uhn7.execute-api.us-east-2.amazonaws.com/test/coins")
+                .put(body)
+                .header("User-Agent", "OkHttp Headers.java")
+                .addHeader("Accept", "application/json; q=0.5")
+                .addHeader("Connection", "keep-alive")
+                .addHeader("Content-Type", "application/json")
+                .build();
+
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-            List<Map<String, Object>> gist =
-                    jsonAdapter.fromJson(response.body().source());
-
-            for (Map<String, Object> coin : gist) {
-                System.out.print(coin.get("id") + " ");
-                System.out.print(coin.get("symbol") + " ");
-                System.out.println(coin.get("current_price") + " ");
-            }
-        }
-    }
-
-    public static void runAllInitCoinList() throws Exception {
-        for(int i = 1; i <=10; i++) {
-            initCoinList(Integer.toString(i));
-        }
-    }
-
-    public static void initCoinList(String num) throws Exception {
-        String filename = "src/main/cloudformation/coin" + num + ".json";
-        File newFile = new File(filename);
-        newFile.createNewFile();
-
-        final OkHttpClient client = new OkHttpClient();
-        final Moshi moshi = new Moshi.Builder().build();
-        Type type = Types.newParameterizedType(List.class, Map.class);
-        final JsonAdapter<List<Map<String, Object>>> jsonAdapter =
-                moshi.adapter(type);
-        Request request = new Request.Builder()
-                .url("https://api.coingecko" +
-                        ".com/api/v3/coins/markets?vs_currency=usd&per_page" +
-                        "=25&page=" + num)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-            List<Map<String, Object>> gist =
-                    jsonAdapter.fromJson(response.body().source());
-
-            JSONObject coinsObject = new JSONObject();
-
-            JSONArray coinsList = new JSONArray();
-
-            coinsObject.put("coins", coinsList);
-
-            for (Map<String, Object> coin : gist) {
-                JSONObject idDetails= new JSONObject();
-                JSONObject nameDetails = new JSONObject();
-                JSONObject priceDetails = new JSONObject();
-                idDetails.put("S", coin.get("symbol"));
-                nameDetails.put("S", coin.get("id"));
-                priceDetails.put("N", coin.get("current_price").toString());
-
-                JSONObject allDetailsObject = new JSONObject();;
-                allDetailsObject.put("id", idDetails);
-                allDetailsObject.put("name", nameDetails);
-                allDetailsObject.put("price", priceDetails);
-
-                JSONObject itemObject = new JSONObject();
-                itemObject.put("Item", allDetailsObject );
-
-                JSONObject putRequestObject = new JSONObject();
-                putRequestObject.put("PutRequest", itemObject);
-
-                coinsList.add(putRequestObject);
-            }
-            Files.write(Paths.get(filename), coinsObject.toJSONString().getBytes());
+            System.out.println(response.body().string());
         }
     }
 }
