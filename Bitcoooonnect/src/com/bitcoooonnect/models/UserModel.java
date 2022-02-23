8f33adf1813dc1ea4c232ee8e2af4e637c698df6
Evan Pollak <pollak.evan@gmail.com>
2022-02-22 19:02:48 -0500

    changed usermodel & Model Converter for AuthToken

--- Bitcoooonnect/src/com/bitcoooonnect/models/UserModel.java
+++ Bitcoooonnect/src/com/bitcoooonnect/models/UserModel.java
@@ -2,11 +2,9 @@ package com.bitcoooonnect.models;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class UserModel {
-    private String email;
-    private String password;
+    private String authToken;
     private Map<String, Double> coins = new HashMap<>();
 
     public UserModel() {
@@ -14,26 +12,17 @@ public class UserModel {
     }
 
     public UserModel(Builder builder) {
-        this.email = builder.email;
-        this.password = builder.password;
+        this.authToken = builder.authToken;
         this.coins = builder.coins;
     }
 
 
-    public String getEmail() {
-        return email;
+    public String getAuthToken() {
+        return authToken;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
     }
 
     public Map<String, Double> getCoins() {
@@ -44,42 +33,14 @@ public class UserModel {
         this.coins = coins;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserModel userModel = (UserModel) o;
-        return getEmail().equals(userModel.getEmail()) && Objects.equals(getPassword(), userModel.getPassword()) && Objects.equals(getCoins(), userModel.getCoins());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getEmail(), getPassword(), getCoins());
-    }
-
-    @Override
-    public String toString() {
-        return "UserModel{" +
-                "id='" + email + '\'' +
-                ", name='" + password + '\'' +
-                ", coins=" + coins +
-                '}';
-    }
-
     public static Builder builder() { return new Builder(); }
 
     public static final class Builder {
-        private String email;
-        private String password;
+        private String authToken;
         private Map<String, Double> coins = new HashMap<>();
 
-        public Builder withEmail(String emailToUse) {
-            this.email = emailToUse;
-            return this;
-        }
-
-        public Builder withPassword(String passwordToUse) {
-            this.password = passwordToUse;
+        public Builder withAuthToken(String authTokenToUse) {
+            this.authToken = authTokenToUse;
             return this;
         }
 
